10acc8fb3e41b0051cfb26916fa0eb5c21d4fe5b
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2023-01-08 13:10:39 +0200

    created static back card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.scss';
-// import { PersonalData } from './components/CardData/CardData';
+import { BackCard } from './components/BackCard/BackCard';
+import { PersonalData } from './components/CardData/CardData';
 import { FrontCard } from './components/FrontCard/FrontCard';
 
 function App() {
   return (
     <section className='payment-section'>
-      {/* <div className='payment-section__container container'>
-        <div className='payment-section__cards'></div>
-        <PersonalData />
-      </div> */}
       <FrontCard />
+      <BackCard />
+      <PersonalData />
     </section>
   );
 }
---
+++ src/components/BackCard/BackCard.js
@@ -0,0 +1,9 @@
+import './BackCard.scss';
+
+export const BackCard = () => {
+  return (
+    <div className="back-card">
+      <p className="back-card__cvc">000</p>
+    </div>
+  );
+};
